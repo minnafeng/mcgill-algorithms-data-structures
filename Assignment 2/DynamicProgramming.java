e251153b5325d88fa68a9d3d825b6a8e9cba48dd
minnafeng <82058067+minnafeng@users.noreply.github.com>
2022-07-21 16:04:42 -0400

    Update and rename DynamicProgramming.java (Q2) to DynamicProgramming.java

--- Assignment 2/DynamicProgramming.java
+++ Assignment 2/DynamicProgramming.java
@@ -1,20 +1,5 @@
 import java.util.*;
 
-/*
-
-Dr. Robert Bruce Banner contacted me in order to help him with his weight training program. In particular, he is interested
-in creating a program that will allow him to lift 1000kg. Dr Banner has several (positive integers less or equal than 1000) weight plates,
-possibly of different weights, and its goal is to add some of the plates to a bar so that it can train with a weight as 
-close as possible to 1000kg. Given the (possible) case where two numbers are equally close to 1000 (e.g., 995 and 1005), 
-Dr Banner will pick the greater one (in this case 1005).
-
-Dr Banner is an excellent coder and he is really good in Dynamic Programming; however, he would be busy in a trip to planet Titan
-and he asked me for help. For this question, you will need to complete the function weight, which receives as a parameter a list of 
-positive integers (where each integer is less than or equal than 1000), denoting the weight of each plate. You can safely assume that
-the length of the list is between 1 and 1000. The function must return one integer, the combined weight closest to 1000.
-
-*/
-
 public class DynamicProgramming {
 
     public static boolean optimizes(int sum, int newWeight){ // returns true if adding new weight optimizes solution
