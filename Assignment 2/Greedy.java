575bfe95b8e310cc63ed149ef16687f6f2f0d2e7
minnafeng <82058067+minnafeng@users.noreply.github.com>
2022-07-21 16:06:33 -0400

    Update and rename Greedy.java (Q3) to Greedy.java

---
+++ Assignment 2/Greedy.java
@@ -0,0 +1,52 @@
+import java.util.*;
+
+public class Greedy {
+    ArrayList<Assignment> Assignments = new ArrayList<Assignment>();
+    int m;
+    int lastDeadline = 0;
+
+    protected Greedy(int[] weights, int[] deadlines, int size) {
+        for (int i=0; i<size; i++) {
+            Assignment homework = new Assignment(i, weights[i], deadlines[i]);
+            this.Assignments.add(homework);
+            if (homework.deadline > lastDeadline) {
+                lastDeadline = homework.deadline;
+            }
+        }
+        m =size;
+    }
+
+    /**
+     *
+     * @return Array where output[i] corresponds to the assignment
+     * that will be done at time i.
+     */
+    public int[] SelectAssignments() {
+
+        //Sort assignments
+        //Order will depend on how compare function is implemented
+        Collections.sort(Assignments, new Assignment());
+
+        // If homeworkPlan[i] has a value -1, it indicates that the
+        // i'th timeslot in the homeworkPlan is empty
+        // homeworkPlan contains the homework schedule between now and the last deadline
+        int[] homeworkPlan = new int[lastDeadline];
+        for (int i=0; i < homeworkPlan.length; ++i) {
+            homeworkPlan[i] = -1;
+        }
+
+        int weightSum = 0; // sum of weights
+        int i;
+        for (i=0; i<Assignments.size(); i++){
+            Assignment a = Assignments.get(i);
+            int j;
+            for (j=a.deadline-1; j>=0; j--) { // place assignment at latest hour possible
+                if (homeworkPlan[j] == -1) {
+                    homeworkPlan[j] = a.number;
+                    break;
+                }
+            }
+        }
+        return homeworkPlan;
+    }
+}
