ec33cb8c5e9ba78c874e913f382f3e89eb622d3a
minnafeng <82058067+minnafeng@users.noreply.github.com>
2022-07-21 16:03:53 -0400

    Update and rename CompleteSearch.java (Q1) to CompleteSearch.java

--- Assignment 2/CompleteSearch.java
+++ Assignment 2/CompleteSearch.java
@@ -1,19 +1,5 @@
 import java.util.*;
 
-/*
-
-We provide a two-dimensional board with some holes on it, where each hole can contain a small ball. 
-During the game, and in each turn, you will be allowed to jump (horizontally or vertically) over an adjacent 
-ball into the empty hole next to the jumped ball in line with it. Once the jump is performed, the ball 
-(which was jumped over) is then removed from the board. The idea of the game is to apply the minimum number 
-of moves (i.e., jumps) to end in a state with a minimum number of balls.
-
-For this question, your task is to develop an algorithm (i.e., a complete search one) that de- termines the 
-minimum number of balls that remains in the board after applying (recursively) the above-mentioned movement. 
-You will also need to report the minimum number of moves required to reach that number of balls.
-
-*/
-
 public class CompleteSearch {
 
     // check board
