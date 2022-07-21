a13abac98c0201de42ced81e55be4363da8c8a98
minnafeng <82058067+minnafeng@users.noreply.github.com>
2022-07-21 03:09:49 -0400

    Add files via upload

---
+++ Assignment 1/OpenAddressing.java
@@ -0,0 +1,95 @@
+// No collaborators
+
+import java.io.*;
+import java.util.*;
+
+public class OpenAddressing {
+    public int m; // number of SLOTS AVAILABLE
+    public int A; // the default random number
+    int w;
+    int r;
+    public int[] Table;
+
+    protected OpenAddressing(int w, int seed, int A) {
+
+        this.w = w;
+        this.r = (int) (w-1)/2 +1;
+        this.m = power2(r);
+        if (A==-1){
+            this.A = generateRandom((int) power2(w-1), (int) power2(w),seed);
+        }
+        else{
+            this.A = A;
+        }
+        this.Table = new int[m];
+        for (int i =0; i<m; i++) {
+            Table[i] = -1;
+        }
+
+    }
+
+    /** Calculate 2^w*/
+    public static int power2(int w) {
+        return (int) Math.pow(2, w);
+    }
+    public static int generateRandom(int min, int max, int seed) {
+        Random generator = new Random();
+        if(seed>=0){
+            generator.setSeed(seed);
+        }
+        int i = generator.nextInt(max-min-1);
+        return i+min+1;
+    }
+    /**Implements the hash function g(k)*/
+    public int probe(int key, int i) {
+        int hashVal = (int) ((int) ((((A * key) % power2(w)) >> (w-r)) + i) % power2(r));
+        return hashVal;
+    }
+
+
+    /**Inserts key k into hash table. Returns the number of collisions encountered*/
+    public int insertKey(int key){
+        int collision = 0;
+        int hashVal; // computed from hash function
+        int i = 0;
+        while (i < m) { // while index less than # slots
+            hashVal = probe(key,i);
+            if (Table[hashVal]<0){ // empty space
+                Table[hashVal] = key;
+                break;
+            }  // else -> collision
+            collision++;
+            i++;
+        }
+        return collision;
+    }
+
+    /**Sequentially inserts a list of keys into the HashTable. Outputs total number of collisions */
+    public int insertKeyArray (int[] keyArray){
+        int collision = 0;
+        for (int key: keyArray) {
+            collision += insertKey(key);
+        }
+        return collision;
+    }
+
+    /**Removes key k into hash table. Returns the number of collisions encountered*/
+    public int removeKey(int key){
+        int collision = 0;
+        int hashVal; // value computed from hash function
+        int i = 0;
+        while (i < m) { // while index less than # slots
+            hashVal = probe(key,i);
+            if (Table[hashVal] == key){ // found key
+                Table[hashVal] = -2; // set distinct value to ensure probe skips for next search
+                break;
+            } else if (Table[hashVal] == -1){ // probed empty slot, key not found
+                collision++;
+                break;
+            }
+            collision++;
+            i++;
+        }
+        return collision;
+    }
+}
